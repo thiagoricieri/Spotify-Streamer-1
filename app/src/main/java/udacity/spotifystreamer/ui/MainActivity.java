96ecfdfde3d3597f1649e36cb677fea4b85441ae
Thiago Ricieri <thiago.ricieri@gmail.com>
2015-05-24 16:08:48 -0300

    Apply IME action to textfield

--- app/src/main/java/udacity/spotifystreamer/ui/MainActivity.java
+++ app/src/main/java/udacity/spotifystreamer/ui/MainActivity.java
@@ -1,40 +1,57 @@
 package udacity.spotifystreamer.ui;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import udacity.spotifystreamer.R;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+	@InjectView(android.R.id.list)
+	ListView listView;
+
+	@InjectView(R.id.type_and_search)
+	EditText textTypeAndSearch;
+
+	private ProgressDialog mProgressDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		ButterKnife.inject(this);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
+		// Apply IME Action
+		textTypeAndSearch.setOnEditorActionListener((example, actionId, event) -> {
+			if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_DOWN) {
+				searchSpotifyArtists(textTypeAndSearch.getText().toString());
+			}
 			return true;
-		}
+		});
+	}
 
-		return super.onOptionsItemSelected(item);
+	private void searchSpotifyArtists(String query){
+
+	}
+
+	private void setLoading(boolean loading){
+		if(loading){
+			mProgressDialog = ProgressDialog.show(this, null, getString(R.string.generic_loading));
+		}
+		else if(mProgressDialog != null){
+			mProgressDialog.dismiss();
+			mProgressDialog = null;
+		}
 	}
 }
