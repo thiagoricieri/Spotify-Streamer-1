6119c4ff4619fa497400fb348323ee85b5892f06
Thiago Ricieri <thiago.ricieri@gmail.com>
2015-05-24 15:54:08 -0300

    Start using packages

--- app/src/main/java/udacity/spotifystreamer/ui/MainActivity.java
+++ app/src/main/java/udacity/spotifystreamer/ui/MainActivity.java
@@ -1,9 +1,11 @@
-package udacity.spotifystreamer;
+package udacity.spotifystreamer.ui;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import udacity.spotifystreamer.R;
 
 
 public class MainActivity extends ActionBarActivity {
